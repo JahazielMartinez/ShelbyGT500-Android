d7d3b29816e68aec94c871278500498d6c0f3c97
Jahaziel Martinez <jefedani45@comunidad.unam.mx>
2021-06-15 12:00:19 -0500

    Agregando Archivos

---
+++ app/src/main/java/com/example/shelbygt/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.example.shelbygt;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+            }
+}
